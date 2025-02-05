f95614ad7aea2d6563f583515f65fe671d75bb21
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-02-05 19:49:09 +0200

    init: push for testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { VolunteersComponent } from './pages/volunteers/volunteers.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+    },
+    {
+        path: 'volunteers',
+        component: VolunteersComponent
     }
 ];
